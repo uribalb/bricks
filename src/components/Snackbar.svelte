4536a4f894b03b1d0c7043507857ce41e9db6008
jakobrosenberg <jakobrosenberg@gmail.com>
2019-11-19 18:54:32 +0100

    snackbar rewrite

--- src/components/Snackbar.svelte
+++ src/components/Snackbar.svelte
@@ -1,64 +1,54 @@
 <script>
   import { onDestroy } from "svelte";
+  import DefaultTemplate from "./_snackbarTemplate.svelte";
   import emotion from "emotion/dist/emotion.umd.min.js";
   import { crossfade, fade } from "svelte/transition";
   import { flip } from "svelte/animate";
   const { css } = emotion;
 
-  export let defaultTime = 5000,
+  export let time = 5000,
     containerClass = css`
       position: fixed;
       bottom: 20px;
       right: 40px;
       text-align: right;
     `,
-    snackClass = css`
-      display: inline-block; 
-      box-shadow: 0 1px 3px 0 rgba(0, 0, 0, 0.1),
-        0 1px 2px 0 rgba(0, 0, 0, 0.06);
-      background: #fff;
-      padding: 4px 16px;
-      margin-top: 12px;
-      font-size: 1.5rem;
-    `,
-    closeClass = css`
-      cursor: pointer;
-      float: left;
-      padding-right: 8px;
-    `,
-    closeHtml = "x",
+    template = DefaultTemplate,
+    templates = {},
     closeable = false,
     reverse = false,
     filter = msg => msg.snack,
-    transition = fade,
     passthrough = false;
 
   let messages = [];
 
   const _info = console.info;
   console.info = function(msg, ...params) {
-    const snack = JSON.parse(JSON.stringify(msg));
-    snack.fn = msg.fn;
-    if (filter(snack)) {
+    if (filter(msg)) {
+      const snack = Object.assign(
+        msg,
+        Object.assign({ time, closeable, template }, msg)
+      );
+      snack.fn = msg.fn;
       snack.id = messages.length;
       snack.show = true;
       snack.timestamp = Date.now();
-      snack.closeable = snack.closeable || closeable;
-
+      snack.close = function() {
+        snack.show = false;
+        messages = messages;
+      };
+      if (typeof snack.template === "string")
+        snack.template = templates[snack.template];
       if (snack.time !== 0) {
         setTimeout(() => {
           snack.show = false;
           messages = messages;
-        }, snack.time || defaultTime);
+        }, snack.time );
       }
       messages = reverse ? [snack, ...messages] : [...messages, snack];
       if (passthrough) _info(msg, ...params);
     } else _info(msg, ...params);
   };
-
-  function close(i) {
-    messages[i].show = false;
-  }
 
   onDestroy(() => {
     // Let's return console.info to its former glory
@@ -69,27 +59,15 @@
 <ul class="{containerClass} snack-container">
   {#each messages.filter(msg => msg.show) as message (message.id)}
     <li
+      style={message.fn ? 'cursor: pointer;' : ''}
       on:click={() => {
         if (message.fn) {
           message.fn();
           message.show = false;
         }
       }}
-      
-      transition:transition
       animate:flip>
-      <span class="snack {snackClass}
-      {message.class || ''}
-      {message.closeable ? 'has-close' : ''}"
-      style={message.fn ? 'cursor: pointer;':''}
-      >
-        {message.snack}
-        {#if message.closeable}
-          <span class="{closeClass} snack-close" on:click={() => close(message.id)}>
-            {@html closeHtml}
-          </span>
-        {/if}
-      </span>
+      <svelte:component this={message.template} bind:msg={message} />
     </li>
   {/each}
 </ul>
---
+++ src/components/_snackbarTemplate.svelte
@@ -0,0 +1,29 @@
+<script>
+  import { fade } from "svelte/transition";
+  export let msg;
+</script>
+
+<style>
+  .snack {
+    display: inline-block;
+    box-shadow: 0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06);
+    background: #fff;
+    padding: 4px 16px;
+    margin-top: 12px;
+    font-size: 1.5rem;
+  }
+  .close {
+    cursor: pointer;
+    float: left;
+    padding-right: 8px;
+  }
+</style>
+
+<div
+  transition:fade
+  class="snack {msg.class || ''}">
+  {msg.snack}
+  {#if msg.closeable}
+    <span class="close" on:click={msg.close}>x</span>
+  {/if}
+</div>
